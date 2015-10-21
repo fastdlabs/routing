c16a5d353214e6373a785cd94da940b4310ddda6
janhuang <384099566@qq.com>
2015-10-21 22:05:10 +0800

    add idea file

---
+++ examples/idea.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Idea:
+ *
+ * Route::get();
+ * Route::list();
+ * Route::list([group name]);
+ * Route::cache();
+ *
+ * $router = new Router();
+ * $router->get('/');
+ * $router->post();
+ *
+ * $router->set('get|post', '/', [])
+ *
+ */
+
