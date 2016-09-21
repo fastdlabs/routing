a80cde3d149b151c338911d80553e671129ced84
janhuang <bboyjanhuang@gmail.com>
2016-09-21 23:42:38 +0800

    update annotation

--- src/Route.php
+++ src/Route.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: janhuang
- * Date: 15/1/29
- * Time: 上午11:46
- * Github: https://www.github.com/janhuang
- * Coding: https://www.coding.net/janhuang
- * SegmentFault: http://segmentfault.com/u/janhuang
- * Blog: http://segmentfault.com/blog/janhuang
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
  */
 
 namespace FastD\Routing;
